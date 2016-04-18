20b212977be7cb044bfb076e40fbccc7a076d9db
ggattipinheiro <ggattipinheiro@mediametrie.fr>
2016-04-18 17:38:24 +0200

    Cleaning up file

--- ep2.py
+++ ep2.py
@@ -1,55 +1,3 @@
-
-class IllegalState(Exception):
-	pass
-
-class IllegalArgument(Exception):
-	pass
-
-# return true if a matrix is square,
-# 		 false otherwise
-#
-# throws IllegalArgumentException if matrix does not have
-# 		 the same number of elements for each row
-def isSquareMatrix(matrix):
-	rows = len(matrix)
-	
-	mit = iter(matrix)
-	first = len(next(mit))
-	while(hasNext(mit)):
-		others = len(next(mit))
-		if(others != first):
-			raise IllegalArgument('Expected a matrix <%s>', matrix)
-	
-	return first == rows
-
-# return a vector that is the result between
-# 		 the product of a square matrix with
-# 		 another vector
-def product(matrix, v):
-	if(not isSquareMatrix(matrix)):
-		raise IllegalArgument('Expected square matrix <%s>', matrix)
-
-	if(len(matrix) != len(v)):
-		raise IllegalArgument('Expected vector to have same size as '+
-				'matrix. vector.size=%d, matrix.size=%d', len(matrix), len(vector))
-	#
-	#TODO: implement me
-	return 0
-
-# return true if both vectors are the same,
-# 		 false otherwise
-#
-# throws IllegalArgumentException if both vectors
-#		 does not have same size
-def equals(v1, v2):
-	if(len(v1) != len(v2)):
-		raise IllegalArgument('Expected vectors of same ' +
-			'size v1.size=%d, v2.size=%d', len(v1), len(v2))
-	#
-	# TODO: implement me
-	return false
-
-
 #Ax=d
 #
 # [2, 1, 0][x1]   [4]
@@ -92,8 +40,3 @@ for i in reversed(range(0, n-1)):
 	x.insert(i, xi)
 
 print "x=" + str(x)
-
-# Main
-
-## Creates a list containing 5 lists initialized to 0
-#Matrix = [[0 for x in range(5)] for x in range(5)] 
